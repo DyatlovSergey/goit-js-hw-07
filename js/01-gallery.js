5f826c8211af3844feb36ec88a5bac2303218582
DyatlovSergey <s.dyatlov90@gmail.com>
2021-11-13 20:06:01 +0200

    создал динамическую разметку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const itemConatainer = document.querySelector(".gallery");
+const itemConatainerMarkup = createGalleryItems(galleryItems);
+console.log(createGalleryItems(galleryItems));
+
+itemConatainer.insertAdjacentHTML("beforeend", itemConatainerMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
